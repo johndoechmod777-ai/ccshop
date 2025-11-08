4fd141a8b00da38aab22c0c90282fae77cd1ed46
johndoechmod777-ai <johndoechmod777@gmail.com>
2025-11-08 09:28:22 -0500

    Add files via upload

---
+++ ccshop/firebase-config.js
@@ -0,0 +1,13 @@
+// firebase-config.js
+const firebaseConfig = {
+    apiKey: "AIzaSyD2tvQjJzh7ukTSyxzIQSo7GSpz7L8z11I",
+    authDomain: "ccshop-realtime.firebaseapp.com",
+    databaseURL: "https://ccshop-realtime-default-rtdb.firebaseio.com",
+    projectId: "ccshop-realtime",
+    storageBucket: "ccshop-realtime.firebasestorage.app",
+    messagingSenderId: "795626673305",
+    appId: "1:795626673305:web:094dc265de1d4d1bc7d408"
+};
+
+// Inicializar Firebase
+firebase.initializeApp(firebaseConfig);
